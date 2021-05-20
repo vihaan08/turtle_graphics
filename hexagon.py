cf83dc0d33540c0c190d3e7782d6d57b6168c42a
vihaan08 <70809815+vihaan08@users.noreply.github.com>
2021-05-20 16:39:21 +0530

    Add files via upload

---
+++ hexagon.py
@@ -0,0 +1,8 @@
+import turtle
+hexagon = turtle.Turtle()
+sides = 6
+angle = 60
+for i in range(6):
+    hexagon.forward(80)
+    hexagon.right(60)
+    turtle.done
---
+++ square.py
@@ -0,0 +1,7 @@
+import turtle
+square = turtle.Turtle()
+for i in range(4):
+    square.forward(50)
+    square.right(90)
+    
+
